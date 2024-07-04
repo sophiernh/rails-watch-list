5947ef6835d62d4b9e0a9ebbbe2fbe79024b0aa6
sophie ranaivoharisoa <sophie.rhf@gmail.com>
2024-07-04 14:46:59 +0200

    seed watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1- require open-uri and json
+require "open-uri"
+require "json"
+# 2- creating a variable with the url
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+
+# 3- Read API
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)
+
+# 4- Iterate on each movie and create a movie in the db
+movies['results'].each do |movie|
+  new_movie = Movie.new
+  new_movie.title = movie['title']
+  new_movie.overview = movie['overview']
+  new_movie.rating = movie['vote_average']
+  new_movie.poster_url = "https://image.tmdb.org/t/p/original#{movie['poster_path']}"
+  puts new_movie
+  new_movie.save!
+end
+
+puts "finished"
